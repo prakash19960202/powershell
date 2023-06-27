eb19e17e4ecc0d14566e9c72b70daff5c0b9e100
prakash19960202 <136340051+prakash19960202@users.noreply.github.com>
2023-06-27 18:30:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "sqlserver1-2" {
     device_name = "sqlserver"
 
     initialize_params {
- 	sourceImage = "projects/groovy-karma-388506/global/images/sqlserver1"
+ 	Image = "projects/groovy-karma-388506/global/images/sqlserver1"
       size = 50
       type = "pd-balanced"
     }
